29de850172ec6096b3f6c69be9a90868994d42c8
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-13 23:10:14 -0300

    feat: Update main.go to use PrintUsuario function

--- abstracao/main.go
+++ abstracao/main.go
@@ -2,16 +2,12 @@ package main
 
 import (
 	"abstracao/usuario"
-	"fmt"
 )
 
 func main() {
 	usuario_comum := usuario.RegistraUsuarioComum("Usuario Commum", "usuario_comum@email.com")
-	fmt.Println("Usuario Comum")
-	fmt.Printf("Nome: %s, Email: %s\n\n", usuario_comum.Nome(), usuario_comum.Email())
-
 	usuario_administrador := usuario.RegistraUsuarioAdministrador("Usuario Administrador", "usuario_administrador@email.com")
 
-	fmt.Println("Usuario Administrador")
-	fmt.Printf("Nome: %s, Email: %s\n", usuario_administrador.Nome(), usuario_administrador.Email())
+	usuario.PrintUsuario(usuario_comum)
+	usuario.PrintUsuario(usuario_administrador)
 }
