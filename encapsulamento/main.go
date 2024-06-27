bb031cdad979abae14182e3e1682bb9c970ce3e3
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-27 11:39:54 -0300

    feat: Refactor main.go to encapsulate account operations

--- encapsulamento/main.go
+++ encapsulamento/main.go
@@ -2,26 +2,35 @@ package main
 
 import "fmt"
 
+// Conta é uma estrutura que representa uma conta bancária.
 type Conta struct {
 	numero int
 	saldo  float64
 }
 
+// Depositar é um método que adiciona um valor ao saldo da conta.
 func (c *Conta) Depositar(valor float64) {
 	c.saldo += valor
 }
 
+// Sacar é um método que subtrai um valor do saldo da conta.
 func (c *Conta) Sacar(valor float64) {
 	c.saldo -= valor
 }
 
+// Saldo é um método que retorna o saldo da conta.
 func (c *Conta) Saldo() float64 {
 	return c.saldo
 }
 
 func main() {
+	// Cria uma nova conta com saldo inicial de 100.0.
 	conta := Conta{numero: 123, saldo: 100.0}
+
+	// Tell, Don't Ask - Princípio de design de software.
+	// Depositar e Sacar são métodos que informam a conta sobre as ações a serem realizadas.
 	conta.Depositar(50.0)
 	conta.Sacar(25.0)
+
 	fmt.Println(conta.Saldo())
 }
